c50a14416b16b4685b86e6cc185afb2d2d874025
Stephen Esser <Stephen.Esser@gmail.com>
2019-11-25 21:22:51 -0600

    Update sidebar resizing logic (#8)
    * Update sidebar resizing logic
    
    * Bump lockfile
    
    * Add min-height to content
    
    * Add min-height to catalog
    
    * Adjust overflow

--- src/components/layers/Layers.jsx
+++ src/components/layers/Layers.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Tree from '../tree/Tree';
@@ -20,54 +20,8 @@ const propTypes = {
 const Layers = (props) => {
   const { selected, workspace } = props;
 
-  const container = useRef();
-
-  /**
-   * Modifies the layers height as the mouse moves.
-   * @param {event} event - The mouse move event.
-   */
-  const handleMouseMove = (event) => {
-    container.current.style.height = `${window.innerHeight - event.clientY}px`;
-  };
-
-  /**
-   * Restores the window state on mouse up.
-   */
-  const handleMouseUp = () => {
-    // Re-enable user selection and pointer events.
-    document.body.classList.remove(cx('inactive'));
-    document.documentElement.style.cursor = '';
-
-    window.removeEventListener('mousemove', handleMouseMove);
-    window.removeEventListener('mouseup', handleMouseUp);
-  };
-
-  /**
-   * Initializes listeners for resizing the layers.
-   * @param {event} event - The mouse down event.
-   */
-  const handleMouseDown = (event) => {
-    // Only accept left mouse clicks.
-    if (event.button > 1) {
-      return;
-    }
-
-    document.documentElement.style.cursor = 'ns-resize';
-
-    // Disable user selection and pointer events to prevent interference during resizing.
-    document.body.className += cx('inactive');
-
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
-  };
-
   return (
-    // eslint-disable-next-line react/forbid-dom-props
-    <div className={cx('layers')} ref={container}>
-      <div className={cx('header')} onMouseDown={handleMouseDown} role="presentation">
-        <div className={cx('resizer')} />
-        Layers
-      </div>
+    <div className={cx('layers')}>
       <Tree workspace={workspace} selected={selected} />
     </div>
   );
