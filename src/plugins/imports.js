7dcf86ef90dc226701cbd9c6396cda6795be7f8c
Stephen Esser <Stephen.Esser@gmail.com>
2019-11-26 17:18:02 -0600

    Add terra-content-container plugin

--- src/plugins/imports.js
+++ src/plugins/imports.js
@@ -1,5 +1,6 @@
 export default {
   'terra-card': () => import('terra-card'),
+  'terra-content-container': () => import('terra-content-container'),
   'terra-list': () => import('terra-list'),
   'terra-overlay': () => import('terra-overlay'),
 };
