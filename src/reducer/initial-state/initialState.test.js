cd346d19bacc0171ca2c390f8ea9a54cd4b272ac
Stephen Esser <Stephen.Esser@Cerner.com>
2020-01-12 12:56:20 -0600

    Update initial state and replace tests

--- src/reducer/initial-state/initialState.test.js
+++ src/reducer/initial-state/initialState.test.js
@@ -3,21 +3,46 @@ import initialState from './initialState';
 jest.mock('uuid/v4', () => () => 'mock-uuid');
 
 describe('Initial State ', () => {
-  it('should export the expected initial workspace state', () => {
-    const { workspace } = initialState;
+  it('should export the placeholder import', () => {
+    const { imports } = initialState;
+
+    expect(imports['terra-sandbox:Placeholder']).toBeDefined();
+  });
+
+  it('should export the canvas import', () => {
+    const { imports } = initialState;
+
+    expect(imports['terra-sandbox:Canvas']).toBeDefined();
+  });
+
+  it('should export the selected component as undefined', () => {
+    const { selected } = initialState;
+
+    expect(selected).toBeUndefined();
+  });
+
+  it('should export an initial canvas', () => {
+    const { canvas } = initialState;
 
     const expected = {
-      root: {
-        'mock-uuid': {
-          id: 'mock-uuid',
-          parent: 'root',
-          type: 'element',
-          value: {
-            name: 'terra-sandbox:Placeholder',
-            props: {
-              expand: {
-                type: 'bool',
-                value: true,
+      id: 'mock-uuid',
+      type: 'element',
+      value: {
+        name: 'terra-sandbox:Canvas',
+        props: {
+          children: {
+            id: 'mock-uuid',
+            parent: 'mock-uuid',
+            type: 'node',
+            value: {
+              'mock-uuid': {
+                id: 'mock-uuid',
+                parent: 'mock-uuid',
+                type: 'element',
+                value: {
+                  name: 'terra-sandbox:Placeholder',
+                  props: {},
+                },
               },
             },
           },
@@ -25,12 +50,6 @@ describe('Initial State ', () => {
       },
     };
 
-    expect(workspace).toEqual(expected);
-  });
-
-  it('should export the expected initial placeholder import', () => {
-    const { imports } = initialState;
-
-    expect(imports['terra-sandbox:Placeholder']).toBeDefined();
+    expect(canvas).toEqual(expected);
   });
 });
